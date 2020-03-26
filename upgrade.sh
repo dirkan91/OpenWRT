3ee52b5da3a627a4df28b6908897f9f0fad94d17
Dirk Postol Dissel <dirk.p.d@protonmail.com>
2020-03-26 22:24:34 +0100

    First commit. Added upgrade script.

---
+++ upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+opkg update
+LIST=$(opkg list-upgradable | cut -d" " -f1)
+NUMBER=$(echo $LIST | wc -w)
+if [ $NUMBER > 0 ]
+then
+  for PACKAGE in $LIST
+  do
+    opkg upgrade $PACKAGE
+  done
+  echo "Upgrade done. $NUMBER packages upgraded"
+else
+  echo "No packages to upgrade"
+fi
